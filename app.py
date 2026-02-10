db0f5b778042ec886fe083598ad6889af2671b5e
Aqsaali7 <aaqsa5411@gmail.com>
2026-02-10 14:50:33 +0500

    update app.py

--- app.py
+++ app.py
@@ -321,6 +321,9 @@ def generate_pdf():
     buffer.seek(0)
     return send_file(buffer, as_attachment=True, download_name="TravelVoucher.pdf")
 
+import os
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
